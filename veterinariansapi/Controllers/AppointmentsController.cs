00891732cdc215570ee4ae15001a60348fd7da21
Valentina Peric <valentinaperic24@gmail.com>
2023-06-28 15:14:08 -0400

    adding project

---
+++ veterinariansapi/Controllers/AppointmentsController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace veterinariansAppointments.Controllers
+{
+    [ApiController]
+    [Route("/appointments")]
+    public class AppointmentsController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AppointmentsController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAppointments()
+        {
+            try
+            {
+                string apiUrl = "https://723fac0a-1bff-4a20-bdaa-c625eae11567.mock.pstmn.io/appointments";
+                HttpClient httpClient = _httpClientFactory.CreateClient();
+
+                httpClient.DefaultRequestHeaders.Accept.Clear();
+                httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await httpClient.GetAsync(apiUrl);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    return Ok(jsonResponse);
+                }
+
+                return StatusCode((int)response.StatusCode, $"Error: {response.StatusCode}");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Error: {ex.Message}");
+            }
+        }
+
+    }
+}
---
+++ veterinariansapi/Program.cs
@@ -0,0 +1,31 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddHttpClient(); 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+//eable CORS
+app.UseCors(builder =>
+{
+    builder.WithOrigins("http://localhost:3000")
+           .AllowAnyMethod()
+           .AllowAnyHeader();
+});
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
